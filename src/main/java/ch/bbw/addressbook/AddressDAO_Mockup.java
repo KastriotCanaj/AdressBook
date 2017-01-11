ecf04f6f059828b69ce1fcb9985af172cb447bfa
5im14lubischoff <5im14lubischoff@gibwintra.ch>
2017-01-11 09:38:34 +0100

    Intzialer Kommit

---
+++ src/main/java/ch/bbw/addressbook/AddressDAO_Mockup.java
@@ -0,0 +1,37 @@
+package ch.bbw.addressbook;
+
+import java.util.List;
+
+public class AddressDAO_Mockup implements AddressDAO {
+
+	@Override
+	public void create(Address address) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Address read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Address> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Address address) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/ch/bbw/test/TestAddress.java
+++ src/test/java/ch/bbw/test/TestAddress.java
@@ -1,20 +1,43 @@
 package ch.bbw.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ch.bbw.addressbook.Address;
+
 //import org.junit.Before;
 //import org.junit.Test;
 
 public class TestAddress {
 
 //	@Before
-//	public void setup() {		
+//	public void setup() {
 //	}
-//	
+//
 //	@Test
 //	public void testConstructor1() {
 //	}
-//	
+//
 //	@Test
 //	public void testConstructor2() {
 //	}
 
+
+	@Test
+	public void testAddressKonschtrucktor() {
+		Address aedressss = new Address(0, "Philp", "Fehrli", "123456789");
+
+		if (!aedressss.getFirstname().equals("Philp")) {
+			fail();
+		}
+
+		if (!aedressss.getLastname().equals("Fehrli")) {
+			fail();
+		}
+
+		if (!aedressss.getPhonenumber().equals("123456789")) {
+			fail();
+		}
+	}
 }
